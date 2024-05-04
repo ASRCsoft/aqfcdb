c8154030911af1277f46a4deff252724fa4d551e
Mark C Beauharnois <mcbdroid@gmail.com>
2024-05-04 18:07:10 -0400

    Update aqfcdb.py

--- aqfcdb.py
+++ aqfcdb.py
@@ -81,7 +81,7 @@ class runManager(object):
         return(self.prg_cfgdata["RunInformation"]["runsuffix"])
 
     def getMaxToStore(self):
-        return(self.prg_cfgdata["RunInformation"]["maxdaystostore"]
+        return(self.prg_cfgdata["RunInformation"]["maxdaystostore"])
     
     def getNumRetro(self):
         return(self.prg_cfgdata["RunInformation"]["nretrodays"])
